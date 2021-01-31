5cecc1492e6bf22d2952b585923e9f1a3dd62a54
rogeriodelphi <rogeriodelphi@gmail.com>
2021-01-31 18:40:21 -0300

    Criando o Model Serializers

--- escola/settings.py
+++ escola/settings.py
@@ -37,10 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'django-filters',
+    'django_filters',
     'rest_framework',
-
     'cursos',
 ]
 
