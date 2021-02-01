df66d63eb04c3ab30fd4a798f0393e8d83849225
rogeriodelphi <rogeriodelphi@gmail.com>
2021-02-01 00:36:10 -0300

    Criando um CRUD genérico com Django REST Framework

--- cursos/views.py
+++ cursos/views.py
@@ -1,35 +1,28 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
 
-class CursoAPIView(APIView):
-    """ API de Cursos """
-    def get(self, request):
-        cursos = Curso.objects.all()
-        serilizer = CursoSerializer(cursos, many=True)
-        return Response(serilizer.data)
+class CursosAPIView(generics.ListCreateAPIView):
+    """Listar ou criar um curso"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
 
-    def post(self, request):
-        serializer = CursoSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """Atualiza ou deletar um curso"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
 
 
-class AvaliacaoAPIView(APIView):
-    """ API de Avaliações """
-    def get(self, request):
-        avaliacoes = Avaliacao.objects.all()
-        serializer = AvaliacaoSerializer(avaliacoes, many=True)
-        return Response(serializer.data)
 
-        def post(self, request):
-            serializer = AvaliacaoSerializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+class AvaliacoesAPIView(generics.ListCreateAPIView):
+    """Listar ou criar uma avaliação"""
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
 
+
+class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """Atualiza ou deletar uma Avaliação"""
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
