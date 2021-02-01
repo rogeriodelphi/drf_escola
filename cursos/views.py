0ee1e3f9b56bb8de96a5b6a6532530f8139bd192
rogeriodelphi <rogeriodelphi@gmail.com>
2021-01-31 23:32:25 -0300

    Criando APIViews para o método HTTP POST

--- cursos/views.py
+++ cursos/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-# from rest_framework import status
+from rest_framework import status
 
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
@@ -13,11 +13,11 @@ class CursoAPIView(APIView):
         serilizer = CursoSerializer(cursos, many=True)
         return Response(serilizer.data)
 
-    # def post(self, request):
-    #     serializer = CursoSerializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     serializer.save()
-    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
+    def post(self, request):
+        serializer = CursoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class AvaliacaoAPIView(APIView):
@@ -27,9 +27,9 @@ class AvaliacaoAPIView(APIView):
         serializer = AvaliacaoSerializer(avaliacoes, many=True)
         return Response(serializer.data)
 
-        # def post(self, request):
-        #     serializer = AvaliacaoSerializer(data=request.data)
-        # serializer.is_valid(raise_exception=True)
-        # serializer.save()
-        # return Response(serializer.data, status=status.HTTP_201_CREATED)
+        def post(self, request):
+            serializer = AvaliacaoSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
