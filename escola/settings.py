6c602998fd2e103ae345a81f6019e18fe3e04730
rogeriodelphi <rogeriodelphi@gmail.com>
2021-02-08 23:38:30 -0300

    Utilizando Paginação com Django REST Framework

--- escola/settings.py
+++ escola/settings.py
@@ -120,14 +120,14 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 
-# DRF
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE':2
 }
-
 
